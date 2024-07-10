9d3c13c394633e32fadb407888b4fc43c223b20d
cheoljin <asddqe111@bbrick.com>
2024-07-10 15:07:27 +0900

    initial commit

---
+++ bmart/src/test/java/com/java/bmart/BmartApplicationTests.java
@@ -0,0 +1,13 @@
+package com.java.bmart;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BmartApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
